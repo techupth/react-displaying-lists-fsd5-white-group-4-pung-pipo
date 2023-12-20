2e878588b269c6c11c4f6e5db4382cc6650f7e39
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-12-21 02:28:59 +0700

    feat(exercise): add completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import MovieList from "./component/MovieList";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        <h1
+          css={css`
+            text-align: center;
+            margin-bottom: 30px;
+            margin-top: 20px;
+          `}
+        >
+          Movie List Section
+        </h1>
+        <MovieList />
       </section>
     </div>
   );
